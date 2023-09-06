fd41ab34869c0527c9624487fb9692a573c95c84
Ashik Kirmani <115408407+Ashikkirmani011@users.noreply.github.com>
2023-09-06 20:34:37 +0530

    Time: 2384 ms (23.43%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+WITH FirstYearSales AS (
+  SELECT
+    s.product_id,
+    MIN(s.year) AS first_year
+  FROM Sales s
+  GROUP BY s.product_id
+)
+
+SELECT
+  fs.product_id,
+  fs.first_year,
+  s.quantity,
+  s.price
+FROM FirstYearSales fs
+JOIN Sales s ON fs.product_id = s.product_id AND fs.first_year = s.year
+ORDER BY fs.product_id;
